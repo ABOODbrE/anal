c730186cf434e563cbad9e3de3b6bc6802a74faf
ABOODbrE <107629349+ABOODbrE@users.noreply.github.com>
2024-06-01 14:15:12 +0300

    Update 1wpfc_ajaxurl.js

--- JS/1wpfc_ajaxurl.js
+++ JS/1wpfc_ajaxurl.js
@@ -3,6 +3,9 @@
 var wpfc_ajaxurl = "https://www.analnice.com/wp-admin/admin-ajax.php";
 var wpfc_nonce = "9047171e6a";
 </script>
-*/
+
 var wpfc_ajaxurl = "https://www.analnice.com/wp-admin/admin-ajax.php";
+var wpfc_nonce = "9047171e6a";
+*/
+var wpfc_ajaxurl = "https://aboodbre.github.io/anal/PHP/admin-ajax.php";
 var wpfc_nonce = "9047171e6a";
